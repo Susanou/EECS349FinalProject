67772abac91c460e508e30eed80278f994a047a3
Fukurou <caos2804@gmail.com>
2019-11-10 11:46:35 -0500

    AI fitted + data used

---
+++ AIstuff/fitting.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+
+#  Copyright (c) 2019, Cameron Hochberg
+#  All rights reserved.
+#
+# Author: Cameron Hochberg
+# Date: 07/2019
+# Homepage: https://github.com/Susanou
+# Email: cam.hochberg@gmail.com
+#
+
+import sys
+import matplotlib.pyplot as plt
+import argparse
+import time
+import pandas as pd
+import numpy as np
+import joblib
+import urllib
+
+from bs4 import BeautifulSoup
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB as naive
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import ComplementNB # as naive
+from sklearn.svm import SVC as svc
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+def get_page(url: str):
+    """Fonction pour recuperer le texte nettoyer d'une page html
+    
+    Parameters
+    ----------
+    url : str
+        Url de la page
+    
+    Returns
+    -------
+    str
+        Renvoi le texte de la page sans le code HTML
+    """
+    response = urllib.request.urlopen("%s" % url)
+    html = response.read()
+    soup = BeautifulSoup(html, "html5lib")
+    text = soup.get_text(strip=True)
+
+    return text
+
+def predictSGD(): 
+
+    start = time.time()
+
+    clf = Pipeline([
+        ('vect', vectorizer),
+        ('clf', SGDClassifier(loss='log', penalty='l2',
+                        alpha=1e-3, random_state=42,
+                        max_iter=5, tol=None))
+    ], verbose=True)
+
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2), (1,3), (1,4), (1,5)],
+        
+        'clf__alpha': (1e-2, 1e-3, 5e-3, 7e-3, 4e-3, 3e-3, 2e-3, 8e-3, 9e-3),
+    }
+
+    gs_clf = GridSearchCV(clf, parameters, cv=5, iid=False, n_jobs=-1)
+    gs_clf.fit(docs_train, y_train)
+
+    print(gs_clf.best_params_)
+
+    y_predicted = gs_clf.predict(docs_test)
+
+    print("END...... total=%0.2f s" %(start-time.time()))
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
+
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+
+    plt.matshow(cm, cmap=plt.cm.jet)
+    #plt.show()
+
+    joblib.dump(gs_clf, "sgd_model.plk")
+
+
+#If using SVC, not able to get the the different proba of each case
+def predictedSVC():
+    start = time.time()
+
+    clf = Pipeline([
+        ('vect', vectorizer),
+        ('clf', svc(tol=1e-3, verbose=0, random_state=42,
+            C=1.0, max_iter=-1, gamma='scale', probability=True))
+    ], verbose = True)
+
+    parameters={
+        'vect__ngram_range': [(1, 1), (1, 2), (1,3), (1,4), (1,5)],
+        'clf__tol':(1e-3, 1e-2, 5e-3, 2e-3, 3e-3,4e-3),
+        'clf__gamma':('auto', 'scale'),
+        'clf__C':(1.0,.1,.2,.3,.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+    }
+
+    gs_clf = GridSearchCV(clf, parameters, cv=5, iid=False, n_jobs=-1)
+    gs_clf.fit(docs_train, y_train)
+
+    y_predicted = gs_clf.predict(docs_test)
+
+    print(gs_clf.best_params_)
+
+    print("End.......... total=%.2f s" % (start - time.time()))
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
+
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+
+    plt.matshow(cm, cmap=plt.cm.jet)
+    #plt.show()
+
+    joblib.dump(gs_clf, "svc_model.plk")
+
+def predictNaiveBayes():
+
+    start = time.time()
+
+    #MultinomialNB Pipeline
+    clf = Pipeline([
+        ('vect', vectorizer),
+        ('clf', naive(alpha=1.0, fit_prior=True))
+    ], verbose=True)
+
+    parameters={
+        'vect__ngram_range': [(1, 1), (1, 2), (1,3), (1,4), (1,5)],
+        'clf__fit_prior': (True, False),
+        'clf__alpha': (1.0, 0.1, 0.5, 2.0, .25, 0.75, 0.002),
+    }
+
+    gs_clf = GridSearchCV(clf, parameters, cv=5, iid=False, n_jobs=-1)
+    gs_clf.fit(docs_train, y_train)
+
+    print(gs_clf.best_params_)
+
+    y_predicted = gs_clf.predict(docs_test)
+
+    print("End.......... total=%.2f s" % (start - time.time()))
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
+
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+    plt.matshow(cm, cmap=plt.cm.jet)
+    #plt.show()
+
+
+    joblib.dump(gs_clf, "naive_model.plk")
+
+def vote(prob1, prob2, prob3):
+    """Fonction nous permettant de voter sur le resulat en cas d'absence de choix
+    
+    Parameters
+    ----------
+    prob1 : array-like
+        liste de probabilite pour chaque classe selon le premier algorithme
+    prob2 : array-like
+        liste de probabilite pour chaque classe selon le deuxieme algorithme
+    prob3 : array-like
+        liste de probabilite pour chaque classe selon le troisieme algorithme
+    
+    Returns
+    -------
+    int
+        Renvoi soit l'indexe du resultat du resultat le plus probable
+    float
+        La  probabilite associee au resultat
+    """
+
+    top1 = np.argsort(prob1, axis=1)[:,-3:]
+    top2 = np.argsort(prob2, axis=1)[:,-3:]
+    top3 = np.argsort(prob3, axis=1)[:,-3:]
+
+    top1 = top1[0]
+    top2 = top2[0]
+    top3 = top3[0]
+
+    #top3 = [0, 0, 0]
+
+    sums = dict()
+
+    if top1[2] == top2[2] and top1[2] == top3[2]:
+        return top1[2], prob1[0][top1[2]]+prob2[0][top1[2]]+prob3[0][top1[2]]
+    elif top1[2] == top2[2]:
+        return top1[2], prob1[0][top1[2]]+prob2[0][top1[2]]+.2
+    elif top1[2] == top3[2]:
+        return top1[2], prob1[0][top1[2]]+prob3[0][top1[2]]+.2
+    elif top2[2] == top3[2]:
+        return top2[2], prob2[0][top1[2]]+prob3[0][top1[2]]+.2
+    else:
+        
+        for i in top1:
+            if i in sums:
+                sums[i] += prob1[0][i]
+            else:
+                sums[i] = prob1[0][i]
+        for j in top2:
+            if j in sums:
+                sums[j] += prob2[0][j]
+            else:
+                sums[j] = prob2[0][j]
+        for k in top3:
+            if k in sums:
+                sums[k] += prob3[0][k]
+            else:
+                sums[k] = prob3[0][k]
+
+        maxV = max(sums.values())
+        maxK = [k for k, v in sums.items() if v == maxV]
+
+        return (maxK[0], maxV)
+
+if __name__ == "__main__":
+    global languages_data_folder
+    global dataset
+    global docs_train, docs_test, y_train, y_test
+    global vectorizer
+
+    parser = argparse.ArgumentParser(description="Script for fitting according to 3 different algorithms:\n SVC, Naive Bayes OR SGD")
+
+    parser.add_argument("-n", "--naive", action="store_true", help='Use the Naive Bayes algorithm')
+    parser.add_argument("-g", '--sgd', action='store_true', help="Use the SVG algorithm")
+    parser.add_argument("-c", '--svc', action="store_true", help="Use the SVC algorithm")
+    parser.add_argument("-a", "--all", action="store_true", help="Compiles all the files for the different algorithms")
+    parser.add_argument("dataPath",default="txt_sentoken",nargs='?', type=str, help="Path to the folder where all the text data is stored")
+
+    args = parser.parse_args()
+
+    languages_data_folder = args.dataPath
+    dataset = load_files(languages_data_folder)
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        dataset.data, dataset.target, test_size=.25, random_state=42, shuffle=True)
+    vectorizer = TfidfVectorizer(ngram_range=(1,1), analyzer='word', use_idf=True)
+
+    if args.naive:
+        print("Using naive bayes to fit")
+        predictNaiveBayes()
+    elif args.sgd:
+        print("using sgd to fit")
+        predictSGD()
+    elif args.svc:
+        print("Using SVC to fit")
+        predictedSVC()
+    elif args.all:
+        print("Compiling all algorithms")
+        print("Using SVC to fit")
+        predictedSVC()
+        print("using SGD to fit")
+        predictSGD()
+        print("Using naive bayes to fit")
+        predictNaiveBayes()
+    else:
+        parser.print_help()
+
+    
