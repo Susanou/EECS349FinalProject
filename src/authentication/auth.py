0cf5c949805176910b0b88e1ff6b046fb52d7576
Emilio Lopez <eil11@case.edu>
2019-11-11 20:15:21 -0500

    More changes

--- src/authentication/auth.py
+++ src/authentication/auth.py
@@ -6,14 +6,17 @@ def obtain_required_cookies(login_url):
     browser = webdriver.Firefox()
     browser.get(login_url)
 
-    time.sleep(5)    # wait for the possible identity check to finish
+    time.sleep(6)    # wait for the possible identity check to finish
 
     usernameElem = browser.find_element_by_name('username')
     usernameElem.send_keys('Napoleon')
 
     passwordElem = browser.find_element_by_name('password')
     passwordElem.send_keys('LillyTheDog1!')
-    passwordElem.submit()
+
+    buttonElemn = browser.find_element_by_name('submit')
+    buttonElemn.submit()
+    
 
     return browser.manage().getCookies()
 
