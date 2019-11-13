62b32887a924d9b2629828e09ade9cd525f5ef0c
Emilio Lopez <eil11@case.edu>
2019-11-13 17:14:10 -0500

    Aggregate scraped thread ids

--- src/main.py
+++ src/main.py
@@ -3,12 +3,22 @@ from authentication.auth import obtain_cookies_and_thread_meta
 search_term = 'fortnite'
 forum_url = 'https://hackforums.net/showthread.php?tid='
 
+def __aggregate_thread_ids(meta_info_list):
+    new_list = []
+    for thread in meta_info_list:
+        new_list.append(thread[0])    # 0th elem is the tid
+    
+    return new_list
+
+
 if __name__ == '__main__':
 
     # this might take some time
     scrape_results = obtain_cookies_and_thread_meta(search_term)
     cookie = scrape_results[0]
-    thread_ids = scrape_results[1]
+    thread_meta = scrape_results[1]
+
+    thread_ids = __aggregate_thread_ids(thread_meta)
     
     # TODO Begin scraping each thread for the information
     for i, thread in enumerate(thread_ids):
